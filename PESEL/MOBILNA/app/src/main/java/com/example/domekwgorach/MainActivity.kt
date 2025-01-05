ab17300346c0df62d127f1f07a880899c2ac8445
Fracek525 <michal.frackiewicz@cyfroweszkoly.pl>
2025-01-05 22:43:42 +0100

    Dodanie funkcjonalności przycisku polub

--- PESEL/MOBILNA/app/src/main/java/com/example/domekwgorach/MainActivity.kt
+++ PESEL/MOBILNA/app/src/main/java/com/example/domekwgorach/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.domekwgorach
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        var likes = 0
+
+        val like: Button = findViewById(R.id.like)
+        val save: Button = findViewById(R.id.save)
+        val delete: Button = findViewById(R.id.delete)
+        val number: TextView = findViewById(R.id.number)
+
+        like.setOnClickListener {
+            likes += 1
+            number.text = "${likes} polubień"
         }
     }
 }
